80d8ed0c2d2759ac4b741693f0ba57553fb29d0c
Francois Sabot <francois.sabot@ird.fr>
2020-06-08 18:47:49 +0200

    Updating Licences

--- svTEidentification.py
+++ svTEidentification.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+###################################################################################################################################
+#
+# Copyright 2019-2020 IRD-CNRS-Lyon1 University
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# You should have received a copy of the CeCILL-C license with this program.
+#If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
+#
+# Intellectual property belongs to authors and IRD, CNRS, and Lyon 1 University  for all versions
+# Version 0.1 written by Francois Sabot
+#
+###############################################################################################################################
 """
 	Identifying complete TE differences between two assemblies using Assemblytics/RaGOO output
 	==========================
@@ -7,7 +35,7 @@
 	:contact: francois.sabot@ird.fr
 	:date: 10/10/2019
 	:version: 0.1
-	:licence: GPLv3
+	:licence: GPLv3 - CeCILL-C
 	Script description
 	------------------
 	svTEidentification.py will inform about the new TE putative insertion based on a Assemblytics BED file.
