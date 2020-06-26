735b36b367779c485d84f16f04dfa77be4fada83
M-D75 <mourdasm13@gmail.com>
2020-06-26 18:33:31 +0200

    delet police

--- pipeline_vrare/scripts_R/hit_map_ET.R
+++ pipeline_vrare/scripts_R/hit_map_ET.R
@@ -2,9 +2,9 @@ args = commandArgs(trailingOnly=TRUE)
 
 library(ggplot2)
 library(RColorBrewer)
-library(extrafont) 
-font_import() 
-loadfonts(device = "postscript")
+#library(extrafont) 
+#font_import() 
+#loadfonts(device = "postscript")
 
 name_file = args[1]
 print(name_file)
@@ -14,9 +14,9 @@ head(data)
 
 data$y <- factor(data$y, levels = data$y)
 
-spl1 = unlist(strsplit(name_file, "/"))
-spl2 = unlist(strsplit(spl1[1], "\\."))
-spl = unlist(strsplit(spl2[1], "_"))
+spl1  = unlist(strsplit(name_file, "/"))
+spl2  = unlist(strsplit(spl1[1], "\\."))
+spl   = unlist(strsplit(spl2[1], "_"))
 
 print(spl[1])
 
@@ -24,14 +24,14 @@ ggplot(data, aes(x, y, fill=z, height=1), width=50) +
   geom_tile()+
 
   theme(axis.text.x = element_blank(),
-          axis.text.y = element_text(family="Helvetica", face="plain", color="#222222", 
+          axis.text.y = element_text(face="plain", color="#222222", 
                            size=12, angle=0))+
   coord_fixed()+
 
   scale_fill_gradientn(breaks = c(0, 25, 50, 75, 93), colours = brewer.pal(n= 9, name="YlOrRd"))+
 
   labs(y='Transposables Element', x='', fill='NEW INSERTION')+
-  theme(text=element_text(family="Helvetica", size=14), plot.title = element_text(family="Hemondalisa", size=30, hjust = 0.5)) 
+  theme(text=element_text(size=14), plot.title = element_text(size=30, hjust = 0.5)) 
 
 ggsave(paste(spl[1], "_hit_map.png", sep=""))
 
