dac0d85c4b80e0c0bfd21f32c9d91b1b02661692
M-D75 <mourdasm13@gmail.com>
2021-04-26 14:54:11 +0200

    recup old load

--- lib/bash/end_load.sh
+++ lib/bash/end_load.sh
@@ -1,5 +1,5 @@
 pid=`ps -f | grep "TrEMOLO/lib/bash/load.sh" | awk '$8=="bash" {print $2}'`
 #echo "PID=${pid}"
-kill -s SIGTERM ${pid} 
-kill -s SIGINT ${pid}
+kill -s 2 ${pid} 
+kill -s 15 ${pid}
 echo 
