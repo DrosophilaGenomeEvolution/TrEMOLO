6d6e5b3fb0b74e6847bc72db2a3c44725b1c6211
M-D75 <mourdasm13@gmail.com>
2022-04-03 00:57:57 +0200

    update

--- lib/bash/end_load.sh
+++ lib/bash/end_load.sh
@@ -1,6 +1,5 @@
 #pid=`ps -f | grep "TrEMOLO/lib/bash/load.sh" | awk '$8=="bash" {print $2}'`
 pid=`ps -f | grep "TrEMOLO/lib/nodejs/load.js" | awk '$8=="node" {print $2}'`
 if [ -n "${pid}" ]; then
-    echo "kill -- ${path_to_work}/.pid"
     kill -s 12 ${pid}
 fi;
