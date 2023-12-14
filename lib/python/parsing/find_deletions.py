becd422b09b2cd0fc3b53a7b8d7d629d3736cd89
M-D75 <mourdasm13@gmail.com>
2023-12-14 14:53:48 +0100

    fix FK GENOME max position

--- lib/python/parsing/find_deletions.py
+++ lib/python/parsing/find_deletions.py
@@ -10,7 +10,8 @@ parser = argparse.ArgumentParser(description="parse bam file to get sequence of 
 #MAIN ARGS
 parser.add_argument("bam_file", metavar='<bam-file>', option_strings=['bam-file'], type=argparse.FileType('r'),
                     help="Input bam file")
-parser.add_argument("position", type=str, help="position of TE")
+parser.add_argument("bed_file", metavar='<bed-file>', type=argparse.FileType('r'),
+                    help="")
 
 #OPTION
 parser.add_argument("-m", "--max-size-diff", dest='window', type=int, default=20,
@@ -24,21 +25,25 @@ name_bamfile = args.bam_file
 window       = args.window
 min_dist     = args.min_dist
 
-
-#format chrom:start-end
-if args.position != None:
-    position = {"chrom": args.position.split(":")[0], "start": args.position.split(":")[1].split("-")[0], "end": args.position.split(":")[1].split("-")[1]}
-    size_TE  = int(position["end"]) - int(position["start"])
-else:
-    position = None
-
 bamfile  = pysam.AlignmentFile(name_bamfile, "rb")
 
-NB_DEL        = 0
-read_names    = []
 
-if position != None :
-    for e, read in enumerate(bamfile.fetch(str(position["chrom"]), int(position["start"]), int(position["end"])) ):
+for line in args.bed_file:
+
+    #format bed requier chom, start, end, name, size, seq
+    chrom   = line.split("\t")[0]
+    start   = int(line.split("\t")[1])
+    end     = int(line.split("\t")[2])
+    name    = line.split("\t")[3]
+    size_TE = end-start
+    # size    = int(line.split("\t")[4])
+    # bed_seq = line.split("\t")[5]
+
+    NB_DEL        = 0
+    read_names    = []
+    #print("NAME: ", name)
+    number_read_support = 1
+    for e, read in enumerate( bamfile.fetch(str(chrom), int(start), int(end)) ):
         start_query     = read.query_alignment_start
         reference_start = read.reference_start
         seq             = read.seq
@@ -59,11 +64,11 @@ if position != None :
                     count_read += tupl[1]
 
                 #if we have found DEL to a good position
-                if tupl[0] == 2 and size_TE-window <= tupl[1] and tupl[1] <= size_TE+window and read_name not in read_names and ( abs((count_ref + reference_start)-int(position["start"])) < min_dist or abs((count_ref + reference_start)-int(position["end"])) < min_dist )  :
+                if tupl[0] == 2 and size_TE-window <= tupl[1] and tupl[1] <= size_TE+window and read_name not in read_names and ( abs((count_ref + reference_start)-int(start)) < min_dist or abs((count_ref + reference_start)-int(end)) < min_dist )  :
                     read_names.append(read_name)
                     NB_DEL += 1
 
-print(NB_DEL)
+    print("\t".join([str(chrom), str(start), str(end), str(name.strip()), str(NB_DEL)]))
 
 
 
